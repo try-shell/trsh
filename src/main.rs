7d14b90bf820bdeb53ae9a50dccdff11760a5f27
heropoo <aiyouyou1000@163.com>
2023-02-11 16:09:24 +0800

    fix error

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,31 @@ use std::{
 fn main() {
     loop {
         print!("> ");
-        stdout().flush();
+        _ = stdout().flush();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
-
         let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
-
-        match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
-                }
-            }
-            "exit" => return,
-            command => {
-                let child = Command::new(command).args(args).spawn();
-                match child {
-                    Ok(mut child) => {
-                        child.wait();
+        //let command = parts.next().unwrap();
+        if let Some(command) = parts.next() {
+            let args = parts;
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
                     }
-                    Err(e) => eprintln!("{}", e),
+                }
+                "exit" => return,
+                command => {
+                    let child = Command::new(command).args(args).spawn();
+                    match child {
+                        Ok(mut child) => {
+                            _ = child.wait();
+                        }
+                        Err(e) => eprintln!("{}", e),
+                    }
                 }
             }
         }
